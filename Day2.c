60f8d20ea7d9179d61d010aefc39690571fefc4a
chandu <32795376+chanduch1999@users.noreply.github.com>
2018-10-02 17:00:26 +0530

    Update Day2.c

--- Day2.c
+++ Day2.c
@@ -1,16 +1,18 @@
-#include <stdio.h>
-int main()
-{
-    int i, n, t1 = 1, t2 = 1, nextTerm,sum = 0;
-    scanf("%d", &n);
-    
-    for (i =1 ; i <=n; ++i)
-    {
-        sum += t1;
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-    }
-    printf("%d",sum);
-    return 0;
-}
+ // prahlad's code
+ #include <stdio.h>
+ 
+ int main()
+ {
+     int i, n, t1 = 1, t2 = 1, nextTerm,sum = 0;
+     scanf("%d", &n);
+     
+     for (i =1 ; i <=n; ++i)
+     {
+         sum =sum+ t1;
+         nextTerm = t1 + t2;
+         t1 = t2;
+         t2 = nextTerm;
+     }
+     printf("%d",sum);
+     return 0;
+ }
